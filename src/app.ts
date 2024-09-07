915c5edab0acf34c17a5fa373295219a0ad68e31
BH23526 <dalibor.stojadinovic@macys.com>
2024-09-07 16:06:13 +0200

    Project home page

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
-// Code goes here!
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+
+    constructor() {
+        // define with different approach but accomplish the same results
+        this.templateElement = <HTMLTemplateElement>document.getElementById("project-input")!;
+        this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.attach();
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement("afterbegin", this.element);
+    }
+}
+
+const newProject = new ProjectInput();
